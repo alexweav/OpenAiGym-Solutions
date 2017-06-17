c74d1ae08d6ce0da7e7413a83015eaee6a2b0cd8
alexweav <alexander.weaver@ttu.edu>
2017-06-17 12:09:04 -0500

    Added simpler model architecture, forward pass

--- Pong-v0.py
+++ Pong-v0.py
@@ -2,7 +2,7 @@ import numpy as np
 import gym
 
 num_games = 1
-hidden_size = 300
+hidden_dim = 300
 learning_rate = 1e-4
 gamma = 0.99
 
@@ -10,8 +10,45 @@ def main():
     env = gym.make('Pong-v0')
     input_dim = 80*80*3
     output_dim = 6 #Action space size
+    model = init_model(input_dim, hidden_dim, output_dim)
+    env = gym.make("Pong-v0")
+    observation = env.reset()
+    for game in num_games:
+        env.render()
+
+#Karpathy's preprocess method.
+#Crops out everything except playing field,
+#Collapses into a single-channel image
+def crop_frame(frame):
+    frame = frame[35:195]
+    frame = downsample(frame)
+    frame[frame == 144] = 0
+    frame[frame == 109] = 0
+    frame[frame != 0] = 1
+    return frame
+
+def downsample(image):
+    image = image[::2, ::2, :]
+    return image
+
+def init_model(input_dim, hidden_dim, output_dim):
+    model = {}
+    model['W1'] = np.random.randn(input_dim, hidden_dim)/np.sqrt(input_dim)
+    model['W2'] = np.random.randn(hidden_dim, 1)/np.sqrt(hidden_dim)
+    return model
+
+def relu(x):
+    x[x<0] = 0
+    return x
 
 def sigmoid(x):
     return 1./(1. + np.exp(-x))
+
+def eval_model(observation, model):
+    hidden_score = np.dot(observation, model['W1'])
+    hidden_act = relu(hidden_score)
+    log_prob = np.dot(hidden_act, model['W2'])
+    prob = sigmoid(log_prob)
+    return prob, hidden_act
 
 main()
