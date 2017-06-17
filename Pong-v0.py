2c847b7dff23b7df04140feab04db411f22a3722
alexweav <alexander.weaver@ttu.edu>
2017-06-17 16:20:13 -0500

    Added reward accumulation and data gathering

--- Pong-v0.py
+++ Pong-v0.py
@@ -1,7 +1,7 @@
 import numpy as np
 import gym
 
-num_games = 1
+num_steps = 1000000
 hidden_dim = 300
 learning_rate = 1e-4
 gamma = 0.99
@@ -12,9 +12,34 @@ def main():
     output_dim = 6 #Action space size
     model = init_model(input_dim, hidden_dim, output_dim)
     env = gym.make("Pong-v0")
+    prev_obs = None
     observation = env.reset()
-    for game in num_games:
+    observations, hidden_states, d_log_ps, d_rewards = [], [], [], []
+    reward_sum = 0.0
+    for step in range(num_steps):
         env.render()
+        obs = crop_frame(observation)
+        obs, prev_obs = advance_observation(obs, prev_obs, input_dim)
+        prob, hidden_act = eval_model(obs, model)
+        action = 2 if np.random.uniform() < prob else 3
+        observations.append(obs)
+        hidden_states.append(hidden_act)
+        target = 1 if action == 2 else 0
+        d_log_ps.append(target - prob)
+        observation, reward, done, _ = env.step(action)
+        reward_sum += reward
+        d_rewards.append(reward)
+
+        if done:
+            episode_observations = np.vstack(observations)
+            episode_hidden_states = np.vstack(hidden_states)
+            episode_d_log_ps = np.vstack(d_log_ps)
+            episode_rewards = np.vstack(d_rewards)
+            observations, hidden_states, d_log_ps, d_rewards = [], [], [], []
+            accumulated_reward = accumulate_rewards(episode_rewards)
+            accumulated_reward -= np.mean(accumulated_reward)
+            accumulated_reward /= np.std(accumulated_reward)
+            episode_d_log_ps *= accumulated_reward
 
 #Karpathy's preprocess method.
 #Crops out everything except playing field,
@@ -25,11 +50,16 @@ def crop_frame(frame):
     frame[frame == 144] = 0
     frame[frame == 109] = 0
     frame[frame != 0] = 1
-    return frame
+    return frame.ravel()
 
 def downsample(image):
     image = image[::2, ::2, :]
     return image
+
+def advance_observation(current_obs, prev_obs, input_dim):
+    obs = current_obs - prev_obs if prev_obs is not None else np.zeros(input_dim)
+    prev_obs = current_obs
+    return obs, prev_obs
 
 def init_model(input_dim, hidden_dim, output_dim):
     model = {}
@@ -50,5 +80,13 @@ def eval_model(observation, model):
     log_prob = np.dot(hidden_act, model['W2'])
     prob = sigmoid(log_prob)
     return prob, hidden_act
+
+def accumulate_rewards(rewards):
+    ar = np.zeros_like(rewards)
+    accumulator = 0
+    for r in reversed(range(0, rewards.size)):
+        accumulator = accumulator * gamma + rewards[r]
+        ar[r] = accumulator
+    return ar
 
 main()
